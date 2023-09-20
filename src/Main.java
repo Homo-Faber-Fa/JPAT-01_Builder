a1c3b1c28d39210d78f84ddb538002d191638372
Homo-Faber-Fa <133135868+Homo-Faber-Fa@users.noreply.github.com>
2023-09-20 15:04:29 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Person mom=new PersonBuilder()
+                    .setName("Анна")
+                    .setSurname("Вольф")
+                    .setAge(31)
+                    .setAddress("Сидней")
+                    .build();
+            Person son=mom.newChildBuilder()
+                    .setName("Антошка")
+                    .setAge(11)
+                    .build();
+            System.out.println("У " + mom + " есть сын, " + son);
+
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            //new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
