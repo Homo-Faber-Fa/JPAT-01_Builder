0220629f2a9fe30ab883210d2b11b90634627ea6
Homo-Faber-Fa <133135868+Homo-Faber-Fa@users.noreply.github.com>
2023-09-20 15:07:19 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         }
 
         try {
-            //new PersonBuilder().setAge(-100).build();
+           
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
